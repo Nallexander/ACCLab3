59bdcd88ef468aaacd0a3b798dd0f10c9a134a18
Nallexander <alexander.e.lind@gmail.com>
2017-09-27 09:30:03 +0200

    Moved file

--- app/views.py
+++ app/views.py
@@ -6,5 +6,6 @@ from flask import Flask, jsonify, request
 def index():
 	data = request.get_json()
 	file = data.get('file')
-	
-	return message
+	pronouns = countPronouns(file)
+	print(pronouns)
+	return pronouns
