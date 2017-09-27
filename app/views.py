8838388d44e76ab96a9bd6dc1ced4663a29daf54
Nallexander <alexander.e.lind@gmail.com>
2017-09-27 09:33:13 +0200

    Fix

--- app/views.py
+++ app/views.py
@@ -1,11 +1,12 @@
 from app import app
 from flask import Flask, jsonify, request
+from read_data_celery import countPronouns
 
 @app.route('/')
 @app.route('/index/<uuid>', methods=['GET'])
 def index():
 	data = request.get_json()
 	file = data.get('file')
-	pronouns = countPronouns(file)
+	pronouns = countPronouns.delay(file)
 	print(pronouns)
 	return pronouns
