23c3677c9e54be445e70c71ad552bb52db4e7c11
Nallexander <alexander.e.lind@gmail.com>
2017-09-27 08:34:49 +0200

    Added JSON output

--- read_data.py
+++ read_data.py
@@ -2,14 +2,14 @@ import os
 import json
 
 
-def cleanString(string):
-	filtered = string.replace(',', '')
-	filtered = filtered.replace('.', '')
-	filtered = filtered.replace('!', '')
-	filtered = filtered.replace('?', '')
-	filtered = filtered.replace(')', '')
-	filtered = filtered.lower()
-	return filtered
+# def cleanString(string):
+# 	filtered = string.replace(',', '')
+# 	filtered = filtered.replace('.', '')
+# 	filtered = filtered.replace('!', '')
+# 	filtered = filtered.replace('?', '')
+# 	filtered = filtered.replace(')', '')
+# 	filtered = filtered.lower()
+# 	return filtered
 
 def registerPronoun(string, pronoun_count):
 	han = len([x for x in string.split() if x == 'han'])
@@ -34,20 +34,42 @@ def isJson(myjson):
     return False
   return True
 
+def updateJsonFile(pronoun_count):
+	with open('output.json', 'r') as json_output:
+		data = json.load(json_output)
+	data['han'] = pronoun_count['han'] + data['han']
+	data['hon'] = pronoun_count['hon'] + data['hon']
+	data['den'] = pronoun_count['den'] + data['den']
+	data['det'] = pronoun_count['det'] + data['det']
+	data['denna'] = pronoun_count['denna'] + data['denna']
+	data['hen'] = pronoun_count['hen'] + data['hen']
+	with open('output.json', 'w+') as json_output:
+		json.dump(data, json_output)
+
+
+
 
 
 my_dir = "/Users/Alex/Dropbox/Programmering/Cloud/Lab3/data/"
 os.chdir(my_dir)
+pronoun_count = {'han': 0, 'hon': 0, 'den': 0, 'det': 0, 'denna': 0, 'hen': 0}
+no_of_tweets = 0
+for filename in os.listdir(os.getcwd()):
+	with open(filename) as json_data:	
+		for json_line in json_data:
+			if isJson(json_line):
+				tweet = json.loads(json_line)
+				# cleaned_string = cleanString(d['text'])
 
-with open('f1c47aa7-5b69-4467-897c-24151649bcf4') as json_data:
-	pronoun_count = {'han': 0, 'hon': 0, 'den': 0, 'det': 0, 'denna': 0, 'hen': 0}
-	for json_line in json_data:
-		if isJson(json_line):
-			d = json.loads(json_line)
-			cleaned_string = cleanString(d['text'])
-			
-			registerPronoun(cleaned_string, pronoun_count)
-	print(pronoun_count)
+				if not tweet['retweeted']:
+					no_of_tweets += 1
+					registerPronoun(tweet['text'], pronoun_count)
+print(pronoun_count)
+print(no_of_tweets)
+my_dir2 = "/Users/Alex/Dropbox/Programmering/Cloud/Lab3/"
+os.chdir(my_dir2)
+updateJsonFile(pronoun_count)
+
 
 
 
