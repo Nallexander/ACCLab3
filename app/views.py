231e6654badcf2188de97c5d511a9e56279fd1a1
Nallexander <alexander.e.lind@gmail.com>
2017-09-27 11:21:28 +0200

    fix

--- app/views.py
+++ app/views.py
@@ -19,7 +19,7 @@ def index():
 	print(pronouns.ready())
 	result = pronouns.result
 	print(result)
-	return('hej')
+	return(result)
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', debug=True)
